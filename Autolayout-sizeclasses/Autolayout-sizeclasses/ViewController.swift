13b654428ad92a6632179aab7ec572ac788a694c
abdulmazeed545 <79141475+abdulmazeed545@users.noreply.github.com>
2021-03-29 11:18:30 +0530

    Add files via upload
    Done master

---
+++ Autolayout-sizeclasses/Autolayout-sizeclasses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autolayout-sizeclasses
+//
+//  Created by Shaik abdul mazeed on 29/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
